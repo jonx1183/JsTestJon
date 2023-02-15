7cf81bf58dca6e2e2f7a9589cc1d850cb4658844
jonx1183 <jonx1183@stud.kea.dk>
2023-02-15 09:28:36 +0100

    Index update and setBG

---
+++ Js/SetBagroundColor.js
@@ -0,0 +1,16 @@
+console.log("SetBagroundColor")
+
+const inp = document.querySelector(".inpColor")
+console.log("inp")
+
+const pbCol = document.querySelector(".pbSetColor")
+console.log("pbCol")
+
+const colPicker = document.querySelector(".colpicker")
+console.log("colPicker")
+
+function setBackGrColor(e){
+    console.log("e")
+    const bdy = document.querySelector("body")
+
+}
